7bea22462052d6f6d8dbb8fac36f98af2f28b1d7
İrem <iremmccebeci@gmail.com>
2025-11-22 18:48:04 +0300

    Entities sınıfı oluşturuldu

---
+++ MyPortfolio/DAL/Entities/Feature.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public int Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MyPortfolio/DAL/Entities/Skills.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Skills
+    {
+        public int SkillId { get; set; }
+        public string Title { get; set; }
+        public int Value { get; set; }
+
+    }
+}
